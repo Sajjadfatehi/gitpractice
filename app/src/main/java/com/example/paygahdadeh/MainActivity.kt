b340cc1d79ae4781a17b6bc57e5d19abd2b1773f
Sajjad.fatehi <sajjadfatehi79@gmail.com>
2020-10-26 15:19:06 +0330

    d from 2

--- app/src/main/java/com/example/paygahdadeh/MainActivity.kt
+++ app/src/main/java/com/example/paygahdadeh/MainActivity.kt
@@ -59,6 +59,7 @@ class MainActivity : AppCompatActivity() {
 // second text from cj 3
     //flaky comment
 
+    //d
 //this is first text from cj2
 // first text from cj 3
 // second text from cj 3
